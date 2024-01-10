4a50b7f45ed18c07de63457bc23f908feedda889
André Lucas <andrelucca99@gmail.com>
2024-01-10 12:55:09 -0300

    feat: implementando campo de busca e seus metodos de filtros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { ContainerComponent } from './componentes/container/container.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
@@ -19,6 +21,7 @@ import agenda from './agenda.json';
   imports: [
     CommonModule,
     RouterOutlet,
+    FormsModule,
     ContainerComponent,
     CabecalhoComponent,
     SeparadorComponent,
@@ -30,10 +33,24 @@ import agenda from './agenda.json';
 export class AppComponent {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
   contatos: Contato[] = agenda;
+  filtroPorTexto: string = '';
+
+  private removeAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  filtrarContatosPorTexto(): Contato[] {
+    if(!this.filtroPorTexto) {
+      return this.contatos;
+    }
+    return this.contatos.filter((contato) => {
+      return this.removeAcentos(contato.nome).toLowerCase().includes(this.removeAcentos(this.filtroPorTexto).toLowerCase());
+    })
+  } 
 
   filtrarContatosPorLetraInicial(letra: string) : Contato[] {
-    return this.contatos.filter((contato) => {
-      return contato.nome.toLowerCase().startsWith(letra);
+    return this.filtrarContatosPorTexto().filter((contato) => {
+      return this.removeAcentos(contato.nome).toLowerCase().startsWith(this.removeAcentos(letra).toLowerCase())
     })
   }
 }
